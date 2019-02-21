83e4da0a937bd42e8b21b7e314965858f95d2715
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-21 14:30:17 +0800

    add state loading when load more

--- src/components/ReadMoreButton.js
+++ src/components/ReadMoreButton.js
@@ -1,12 +1,41 @@
 import React from "react";
+import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
 
-const ReadMoreButton = ({to, onClick}) => (
-    <div className="text-center">
-        { to ? 
-        ( <Link to={to} className="button button__read-more">See more</Link> ) : 
-        ( <button className="button button__read-more" onClick={onClick}>See more</button> ) }
-    </div>
-)        
+class ReadMoreButton extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: false
+        }
+    }
+    handleClick = async() => {
+        this.setState({loading: true})
+        await this.props.onClick()
+        this.setState({loading: false})
+    }
+    render() {
+        const {to} = this.props
+        return (
+            <div className="text-center">
+                { to ? 
+                ( <Link to={to} className="button button__read-more">See more</Link> ) : 
+                    this.state.loading ? 
+                    ( 
+                        <div className="text-center">
+                            <img src={require('../assets/loading.svg')} alt="loading" className="loading__image"/>
+                        </div>
+                    ) :
+                    ( <button className="button button__read-more" onClick={this.handleClick}>See more</button> ) 
+                }
+            </div>
+        )        
+    }
+}
     
 export default ReadMoreButton;
+
+ReadMoreButton.propTypes = {
+    to: PropTypes.string,
+    onClick: PropTypes.func
+}
