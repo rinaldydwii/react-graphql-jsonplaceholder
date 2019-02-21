8244e8edd042db489f5f8a2b618d65f259f45222
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-22 00:03:41 +0800

    add loading image & visible props in read more component

--- src/components/ReadMoreButton.js
+++ src/components/ReadMoreButton.js
@@ -15,15 +15,15 @@ class ReadMoreButton extends React.Component {
         this.setState({loading: false})
     }
     render() {
-        const {to} = this.props
+        const {to, visible = true} = this.props
         return (
-            <div className="text-center">
+            <div className={`text-center${visible ? "" : " hidden"}`}>
                 { to ? 
                 ( <Link to={to} className="button button__read-more">See more</Link> ) : 
                     this.state.loading ? 
                     ( 
                         <div className="text-center">
-                            <img src={require('../assets/loading.svg')} alt="loading" className="loading__image"/>
+                            <img src={require('../assets/loading.svg')} alt="loading" className="loading__read-more-image"/>
                         </div>
                     ) :
                     ( <button className="button button__read-more" onClick={this.handleClick}>See more</button> ) 
@@ -36,6 +36,7 @@ class ReadMoreButton extends React.Component {
 export default ReadMoreButton;
 
 ReadMoreButton.propTypes = {
+    visible: PropTypes.bool,
     to: PropTypes.string,
     onClick: PropTypes.func
 }
