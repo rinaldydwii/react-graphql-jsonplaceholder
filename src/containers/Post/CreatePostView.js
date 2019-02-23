5a5693ee63d8ff7692147361e37af4c651d492b8
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-23 22:43:43 +0800

    update validation in create post view

--- src/containers/Post/CreatePostView.js
+++ src/containers/Post/CreatePostView.js
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 import { View } from "../../components";
 import { redirect } from "../../history";
+import { getValidationError, isFormValid } from "../../validations";
 
 const CREATE_POST = gql `
     mutation CreatePost($post: PostInput!) {
@@ -16,11 +17,24 @@ class CreatePostView extends Component {
     constructor() {
         super()
         this.state = {
-            userId: null
+            userId: null,
+            title: null,
+            body: null
         }
+        this.handleChangeTitle.bind(this)
+        this.handleChangeBody.bind(this)
+        this.input = {}
     }
     componentDidMount() {
         this.setState({userId: this.props.match.params.id})
+    }
+    handleChangeTitle = (e) => {
+        const title = e.target.value
+        this.setState({title})
+    }
+    handleChangeBody = (e) => {
+        const body = e.target.value
+        this.setState({body})
     }
     render() {
         const id = this.state.userId
@@ -39,31 +53,46 @@ class CreatePostView extends Component {
                                 <form className="form"
                                     onSubmit={async (e) => {
                                         e.preventDefault()
-                                        await createPost({
-                                            variables: {
-                                                post: {
-                                                    userId: id,
-                                                    title: e.target.title.value,
-                                                    body: e.target.body.value
+                                        if (isFormValid(e, this.input))
+                                            await createPost({
+                                                variables: {
+                                                    post: {
+                                                        userId: id,
+                                                        title: e.target.title.value,
+                                                        body: e.target.body.value
+                                                    }
                                                 }
-                                            }
-                                        })
+                                            })
                                     }}
                                 >
-                                    <div className="form__row">
+                                    <div className={`form__row ${getValidationError('title', this.state.title).className}`}>
                                         <label htmlFor="title">Title</label>
-                                        <input 
-                                            placeholder="Title" 
-                                            name="title" 
-                                            type="text" 
-                                        />
+                                        <div className="form__group">
+                                            <input 
+                                                placeholder="Title" 
+                                                name="title" 
+                                                onChange={this.handleChangeTitle}
+                                                onFocus={this.handleChangeTitle}
+                                                ref={input => this.input["title"] = input}
+                                                type="text" 
+                                            />
+                                            {getValidationError('title', this.state.title).dom}
+                                        </div>
+                                        
                                     </div>
-                                    <div className="form__row">
+                                    <div className={`form__row ${getValidationError('body', this.state.body).className}`}>
                                         <label htmlFor="body">Body</label>
-                                        <textarea 
-                                            placeholder="Body" 
-                                            name="body" 
-                                        />
+                                        <div className="form__group">
+                                            <textarea 
+                                                className={getValidationError('body', this.state.body).className}
+                                                placeholder="Body" 
+                                                name="body" 
+                                                ref={input => this.input["body"] = input}
+                                                onChange={this.handleChangeBody}
+                                                onFocus={this.handleChangeBody}
+                                            />
+                                            {getValidationError('body', this.state.body).dom}
+                                        </div>
                                     </div>
                                     <div className="form__row">
                                         <div className="form__space"></div>
